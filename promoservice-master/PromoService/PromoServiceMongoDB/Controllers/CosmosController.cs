e1f9219d3ebd0fbed077f2bfa7d1581bd872ffe6
unknown <jahnavialapati7@gmail.com>
2019-07-19 06:25:51 +0000

    Adds promoservice in dcp project using azure functions-serverless

---
+++ promoservice-master/PromoService/PromoServiceMongoDB/Controllers/CosmosController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PromoServiceMongoDB.Model;
+using PromoServiceMongoDB.DataAccess;
+using PromoServiceMongoDB.DataAccess.Utility;
+using Microsoft.Azure.WebJobs;
+using Microsoft.AspNetCore.Http;
+
+
+namespace PromoServiceMongoDB.Controllers
+{
+
+    [Route("/v1/promotions")]
+    [ApiController]
+    public class CosmosController : ControllerBase
+    {
+        ICosmosDataAdapter _adapter;
+
+
+
+
+        /* public CosmosController()
+         {
+         }*/
+
+        public CosmosController(ICosmosDataAdapter adapter)
+        {
+            _adapter = adapter;
+        }
+
+       
+
+       
+
+        [HttpPost]
+        [Route("action")]
+        public async Task<IActionResult> CreateAction([FromBody] ProductPromoAction productpromoaction)
+        {
+
+            var result = await _adapter.CreateAction("PromoDatabase", "ProductPromoAction", productpromoaction);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDocument([FromBody] ProductPromo productpromo)
+        {
+            var result = await _adapter.CreateDocument("PromoDatabase", "ProductPromo", productpromo);
+            return Ok(result);
+        }
+
+       /* [HttpPut]
+        [Route("id/action")]
+        public async Task<IActionResult> UpdateAction([FromBody] ProductPromoAction productpromoaction)
+        {
+            var result = await _adapter.updateDocumentAsyncAction("PromoDatabase", "ProductPromoAction", productpromoaction);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var result = await _adapter.DeleteUserAsync("PromoDatabase", "ProductPromo", id);
+            return Ok(result);
+        }*/
+
+
+        // [HttpGet]
+        [FunctionName(nameof(Get))]
+        public async Task<IActionResult> Get([HttpTrigger("get")] HttpRequest request)
+        {
+            var result = await _adapter.GetDataAsync("PromoDatabase", "ProductPromo");
+            return Ok(result);
+        }
+
+       /* [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            var result = await _adapter.GetBy("PromoDatabase", "ProductPromo",id);
+            return Ok(result);
+        }*/
+
+    }
+
+    internal class ApiControllerAttribute : Attribute
+    {
+    }
+}
---
+++ promoservice-master/PromoService/PromoServiceMongoDB/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using PromoServiceMongoDB.DataAccess;
+using PromoServiceMongoDB.DataAccess.Utility;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Azure.WebJobs;
+using PromoServiceMongoDB;
+
+[assembly: WebJobsStartup(typeof(Startup))]
+namespace PromoServiceMongoDB
+{
+    public class Startup : IWebJobsStartup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddMvcCore()
+                 .AddJsonFormatters();
+
+            services.AddTransient<ICosmosConnection, CosmosConnection>();
+            services.AddTransient<ICosmosDataAdapter, CosmosDataAdapter>();
+
+
+          // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /* public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+         {
+             if (env.IsDevelopment())
+             {
+                 app.UseDeveloperExceptionPage();
+             }
+
+             app.UseMvc();
+         }*/
+
+        public void Configure(IWebJobsBuilder builder)
+          { 
+            
+               ConfigureServices(builder.Services); 
+         }
+
+}
+}
